5ec9298fb7f621c2eb503b4fe1b81a87c1ae4e0c
debiansid <9624961+debiansid@users.noreply.github.com>
2025-05-23 18:45:08 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,5 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git wongsyrone https://github.com/homerouter/packages-1' >>feeds.conf.default
+#echo 'src-git wongsyrone https://github.com/homerouter/packages-1' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
